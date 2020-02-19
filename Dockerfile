cf8900ffe19a218388bd526deeafa36a6ba50d84
printscreensys <48695590+printscreensys@users.noreply.github.com>
2020-02-20 01:27:50 +0300

    First working Docker-Container with prediction
    Score 252

---
+++ Dockerfile
@@ -0,0 +1,9 @@
+FROM continuumio/anaconda3:latest
+
+CMD mkdir /opt/results
+WORKDIR /opt/results
+RUN pip install catboost
+COPY model.cbm model.cbm
+COPY baseline_always_zero.py baseline_always_zero.py
+
+CMD python baseline_always_zero.py /tmp/data/test.tsv > /opt/results/result.tsv
---
+++ baseline_always_zero.py
@@ -0,0 +1,19 @@
+import sys
+from functools import partial
+import pandas as pd
+from scipy import stats
+from catboost import CatBoostRegressor as reg
+
+def load_tasks(tasks_filename):
+    return pd.read_csv(tasks_filename, sep="\t")
+
+def main():
+    tests_filename = sys.argv[1]
+    tasks = load_tasks(tests_filename)
+    tasks = tasks[['cpm','hour_start','hour_end','audience_size']]
+    model = reg().load_model('model.cbm')
+    pd.DataFrame(data = abs(model.predict(tasks)), columns = [['at_least_one', 'at_least_two', 'at_least_three']]).to_csv(sys.stdout, sep="\t", index=False, header=True)
+
+
+if __name__ == '__main__':
+    main()
