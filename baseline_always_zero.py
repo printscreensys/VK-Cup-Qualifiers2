ef0bcebb205438e216eb10c018e269c215f33782
printscreensys <ilyasov.yasha@hotmail.com>
2020-02-23 22:44:37 +0300

    Score 118

--- baseline_always_zero.py
+++ baseline_always_zero.py
@@ -10,9 +10,16 @@ def load_tasks(tasks_filename):
 def main():
     tests_filename = sys.argv[1]
     tasks = load_tasks(tests_filename)
+    pub_count = [len(tasks.publishers[i].split(',')) for i in range(len(tasks))]
+
+    tasks = tasks[['cpm','hour_start','hour_end','audience_size']]
     tasks['delta_hour'] = tasks['hour_end'] - tasks['hour_start']
     tasks['cpm*hour'] = tasks['cpm']*tasks['delta_hour']
-    tasks = tasks[['cpm','hour_start','hour_end','audience_size','delta_hour','cpm*hour']]
+    tasks['cpm*pub'] = tasks['cpm']*pd.Series(pub_count)
+
+
+    tasks = tasks[['hour_start','audience_size','delta_hour','cpm*hour','cpm*pub']]
+
     model = reg().load_model('model.cbm')
     pd.DataFrame(data = abs(model.predict(tasks)),columns = [['at_least_one', 'at_least_two', 'at_least_three']]).to_csv(sys.stdout, sep="\t", index=False, header=True)
 
